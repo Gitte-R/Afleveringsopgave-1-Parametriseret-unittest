8815266e4b4786b0d6bc79e80821f75f75be7f89
Gitte-R <gittepige@gmail.com>
2024-09-18 23:23:08 +0200

    Add project files.

---
+++ Afleveringsopgave 1 - Parametriseret unittest/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+public class TransportPris
+{
+    public double BeregnTransportpris(double afstand, double vægt)
+    {
+        if (afstand < 5)
+        {
+            if (vægt < 10)
+            {
+                return 0;
+            }
+            else
+            {
+                return 50;
+            }
+        }
+        else 
+        {
+            if (vægt < 10)
+            {
+                return 75;
+            }
+            else
+            {
+                return 100;
+            }
+        }
+    }
+}
---
+++ Afleveringsopgave 1 - Parametriseret unittest/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Afleveringsopgave_1___Parametriseret_unittest
+{
+    public class Test()
+    {
+        [Theory]
+        [InlineData(2, 2.5, 0)]
+        [InlineData(4, 10, 50)]
+        [InlineData(5, 9, 75)]
+        [InlineData(10, 10, 100)]
+        public void BeregnTransportPris(double afstand, double vægt, double pris)
+        {
+            TransportPris Pris1 = new TransportPris();
+
+            Assert.Equal(Pris1.BeregnTransportpris(afstand, vægt), pris);
+        }   
+    }
+}
